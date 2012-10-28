02a4e43aa9631c91c1c0f3929eceba9dddcb7054
Matt Schwager <schwag09@gmail.com>
2012-10-28 18:00:44 -0400

    Added PE26. Needs explanation.

---
+++ PE26/pe26.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+# http://mathworld.wolfram.com/DecimalExpansion.html
+
+def decimal_expansion_cycle(n):
+	remainders = []
+	for i in range(n):
+		if 10**i % n not in remainders:
+			remainders.append(10**i % n)
+		else:
+			return i - remainders.index(10**i % n)
+
+def main():
+	print max([decimal_expansion_cycle(i) for i in range(1,1000)])
+
+if __name__ == "__main__":
+	main()
